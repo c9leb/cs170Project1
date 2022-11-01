c97bd7ff1c688033dd1b86f8e204884e73101799
caleb <iriiises@gmail.com>
2022-11-01 12:01:19 -0700

    created node.py for working with nodes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
-import treelib
 import heapq as min_heap_esque_queue
+import queue
+import node
 
 # because it sort of acts like a min heap
 # Below are some built in puzzles to allow quick testing.
@@ -24,23 +25,22 @@ impossible = [[8, 7, 1],
               [6, 0, 2],
               [5, 4, 3]]
 
-oh_boy = [[8, 7, 1],
+really_hard = [[8, 7, 1],
           [6, 0, 2],
           [5, 4, 3]]
 
 eight_goal_state = [[1, 2, 3],
                     [4, 5, 6],
                     [7, 8, 0]]
-
 def main():
     
     puzzle_mode = input("Welcome to an 8 Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own." + '\n')
     if puzzle_mode == "1":
         select_and_init_algorithm(init_default_puzzle_mode())
     if puzzle_mode == "2":
-        print("Enter your puzzle, using a zero to represent the blank. " + 
-              "Please only enter valid 8-puzzles. Enter the puzzle demilimiting " + 
-              "the numbers with a space. RET only when finished." + '\n')
+        print("Enter your puzzle, using a zero to represent the blank. "
+              "Please only enter valid 8-puzzles. Enter the puzzle demilimiting "
+              "the numbers with a space. RET only when finished.\n")
         
         puzzle_row_one = input("Enter the first row: ")
         puzzle_row_two = input("Enter the second row: ")
@@ -56,11 +56,12 @@ def main():
         
         user_puzzle = [puzzle_row_one, puzzle_row_two, puzzle_row_three]
         select_and_init_algorithm(user_puzzle)
-    return
+        
+    
 
 def init_default_puzzle_mode():
-    selected_difficulty = input(
-        "You wish to use a default puzzle. Please enter a desired difficulty on a scale from 0 to 5." + '\n')
+    selected_difficulty = input("You wish to use a default puzzle. Please enter a desired difficulty on a scale from 0 to 5." + '\n')
+    
     if selected_difficulty == "0":
         print("Difficulty of 'Trivial' selected.")
         return trivial
@@ -74,48 +75,24 @@ def init_default_puzzle_mode():
         print("Difficulty of 'Doable' selected.")
         return doable
     if selected_difficulty == "4":
-        print("Difficulty of 'Oh Boy' selected.")
-        return oh_boy
+        print("Difficulty of 'Really Hard' selected.")
+        return really_hard
     if selected_difficulty == "5":
         print("Difficulty of 'Impossible' selected.")
         return impossible
 
-def print_puzzle(puzzle):
-    for i in range(0, 3):
-        print(puzzle[i])
-    print('\n')
-
-
 def select_and_init_algorithm(puzzle):
-    algorithm = input("Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, "
-                      "or (3) the Manhattan Distance Heuristic." + '\n')
-    if algorithm == "1":
-        uniform_cost_search(puzzle, 0)
-    if algorithm == "2":
-        uniform_cost_search(puzzle, 1)
+    algorithm = input("Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) the Manhattan Distance Heuristic.\n")
+    if algorithm in ["1", "2", "3"]:
+        uniform_cost_search(puzzle, int(algorithm))
+
 def uniform_cost_search(puzzle, heuristic):
     
-    starting_node = 3
-    working_queue = []
-    repeated_states = dict()
-    min_heap_esque_queue.heappush(working_queue, starting_node)
-    num_nodes_expanded = 0
-    max_queue_size = 0
-    repeated_states[starting_node.board_to_tuple()] = "This is the parent board"
-    
-    stack_to_print = [] # the board states are stored in a stack
-    while len(working_queue) > 0:
-        max_queue_size = max(len(working_queue), max_queue_size)
-        # the node from the queue being considered/checked
-        node_from_queue = min_heap_esque_queue.heappop(working_queue)
-        repeated_states[node_from_queue.board_to_tuple()] = "This can be anything"
         
-        if node_from_queue.solved(): # check if the current state of the board is the solution
-            
-            while len(stack_to_print) > 0: # the stack of nodes for the traceback
-                print_puzzle(stack_to_print.pop())
-                
-            print("Number of nodes expanded:", num_nodes_expanded)
-            print("Max queue size:", max_queue_size)
-            return node_from_queue
-        stack_to_print.append(node_from_queue.board)
+def print_puzzle(puzzle):
+    for i in range(0, 3):
+        print(f"\n{puzzle[i]}")
+    print('\n')
+
+if __name__ == "__main__":
+    main()
---
+++ node.py
@@ -0,0 +1,24 @@
+#Using node objects to create trees for the puzzles
+#Made make_children to create all children from given node, includes move checking and four move operators
+#Included functions to calculate the heuristic costs for misplaced tiles and manhattan distance. 
+import copy
+
+class Node:
+    def __init__(self, puzzle, depth, cost):
+        self.puzzle = puzzle
+        self.depth = depth
+        self.cost = cost
+    
+    def make_children(self):
+        
+        children = []
+        temp = 0
+        curr_row = 0
+        curr_column = 0
+        
+        temp = newNode[2323][23] 
+        
+        
+    def misplaced_tiles(self):
+        
+    def manhattan(self):
