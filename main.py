1f160452f471637264a6b6f73b4a1f9f8751138d
caleb <iriiises@gmail.com>
2022-11-02 21:28:02 -0700

    Code is working and runs correctly

--- main.py
+++ main.py
@@ -1,98 +1,104 @@
-
-import heapq as min_heap_esque_queue
 import queue
-import node
+from node import Node
+import time
 
 # because it sort of acts like a min heap
 # Below are some built in puzzles to allow quick testing.
-trivial = [[1, 2, 3],
-           [4, 5, 6],
-           [7, 8, 0]]
-
-veryEasy = [[1, 2, 3],
-            [4, 5, 6],
-            [7, 0, 8]]
-
-easy = [[1, 2, 0],
-        [4, 5, 3],
-        [7, 8, 6]]
-
-doable = [[0, 1, 2],
-          [4, 5, 3],
-          [7, 8, 6]]
-
-impossible = [[8, 7, 1],
-              [6, 0, 2],
-              [5, 4, 3]]
-
-really_hard = [[8, 7, 1],
-          [6, 0, 2],
-          [5, 4, 3]]
-
-eight_goal_state = [[1, 2, 3],
-                    [4, 5, 6],
-                    [7, 8, 0]]
+# because it sort of acts like a min heap
+# Below are some built in puzzles to allow quick testing.
 def main():
+    puzzle_row = []
+    user_puzzle = []
+    puzzle_mode = int(input("What size puzzle would you like to use?" + '\n'))
+    print(f"Enter your puzzle, pressing space after each single number and pressing enter when you have entered {puzzle_mode} numbers. Please only enter valid {puzzle_mode*puzzle_mode-1}-puzzles.")
     
-    puzzle_mode = input("Welcome to an 8 Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own." + '\n')
-    if puzzle_mode == "1":
-        select_and_init_algorithm(init_default_puzzle_mode())
-    if puzzle_mode == "2":
-        print("Enter your puzzle, using a zero to represent the blank. "
-              "Please only enter valid 8-puzzles. Enter the puzzle demilimiting "
-              "the numbers with a space. RET only when finished.\n")
+    for i in range(puzzle_mode):
+        puzzle_row.append(input(f"Enter row {i+1}:"))
+        puzzle_row[i] = puzzle_row[i].split()
         
-        puzzle_row_one = input("Enter the first row: ")
-        puzzle_row_two = input("Enter the second row: ")
-        puzzle_row_three = input("Enter the third row: ")
-        puzzle_row_one = puzzle_row_one.split()
-        puzzle_row_two = puzzle_row_two.split()
-        puzzle_row_three = puzzle_row_three.split()
+    for i in range(0, puzzle_mode):
+        for j in range(0, puzzle_mode):
+            puzzle_row[i][j] = int(puzzle_row[i][j])
+            
+    for i in range(0, puzzle_mode):    
+        user_puzzle.append(puzzle_row[i])
         
-        for i in range(0, 3):
-            puzzle_row_one[i] = int(puzzle_row_one[i])
-            puzzle_row_two[i] = int(puzzle_row_two[i])
-            puzzle_row_three[i] = int(puzzle_row_three[i])
-        
-        user_puzzle = [puzzle_row_one, puzzle_row_two, puzzle_row_three]
-        select_and_init_algorithm(user_puzzle)
+    select_and_init_algorithm(user_puzzle, puzzle_mode)
         
     
 
-def init_default_puzzle_mode():
-    selected_difficulty = input("You wish to use a default puzzle. Please enter a desired difficulty on a scale from 0 to 5." + '\n')
-    
-    if selected_difficulty == "0":
-        print("Difficulty of 'Trivial' selected.")
-        return trivial
-    if selected_difficulty == "1":
-        print("Difficulty of 'Very Easy' selected.")
-        return veryEasy
-    if selected_difficulty == "2":
-        print("Difficulty of 'Easy' selected.")
-        return easy
-    if selected_difficulty == "3":
-        print("Difficulty of 'Doable' selected.")
-        return doable
-    if selected_difficulty == "4":
-        print("Difficulty of 'Really Hard' selected.")
-        return really_hard
-    if selected_difficulty == "5":
-        print("Difficulty of 'Impossible' selected.")
-        return impossible
+def select_and_init_algorithm(puzzle, dim):
+    algorithm = input("Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, "
+                      "or (3) the Manhattan Distance Heuristic.\n")
+    if algorithm in ("1", "2", "3"):
+        uniform_cost_search(puzzle, int(algorithm)-1, dim)
 
-def select_and_init_algorithm(puzzle):
-    algorithm = input("Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) the Manhattan Distance Heuristic.\n")
-    if algorithm in ["1", "2", "3"]:
-        uniform_cost_search(puzzle, int(algorithm))
-
-def uniform_cost_search(puzzle, heuristic):
-    
+def uniform_cost_search(puzzle, heuristic, dim):
+    time1 = time.time()
+    goal = Node.goal(dim)
+    newNode = Node(puzzle)
+    if(heuristic == 0):
+        newNode.heur = 0
+    if(heuristic == 1):
+        newNode.heur = newNode.misplaced_tiles(goal)
+    if(heuristic == 2):
+        newNode.heur = newNode.manhattan(goal)
         
-def print_puzzle(puzzle):
-    for i in range(0, 3):
-        print(f"\n{puzzle[i]}")
-    print('\n')
+    working_queue = [newNode]
+    repeated_states = []
+    repeated_states.append(working_queue[0].puzzle)
+    num_nodes_expanded = 0
+    max_queue_size = 1
+    queue_size = 1
+    print()
+    
+    while True:
+        
+        check_node = working_queue.pop(0)
+    
+        print(f"The best node to expand with a g(n) of {check_node.depth} and h(n) of {check_node.heur} is...")
+        print_puzzle(check_node.puzzle, dim)
+        
+        if(check_node.solved(goal)):
+            time2 = time.time()
+            print()
+            print("Goal Found!")
+            print(f"Solution depth was: {check_node.depth}")
+            print(f"Number of nodes expanded: {num_nodes_expanded}")
+            print(f"Max queue size: {max_queue_size}\n")
+            print(f"Time: {round(time2 - time1, 3)}\n")
+            return
+        
+        check_node.make_children(repeated_states, heuristic, goal)
+        num_nodes_expanded+=1
+        
+        if(check_node.childup != 0):
+            working_queue.append(check_node.childup)
+            repeated_states.append(check_node.childup.puzzle)
+            
+        if(check_node.childdown != 0):
+            working_queue.append(check_node.childdown)
+            repeated_states.append(check_node.childdown.puzzle)
+            
+        if(check_node.childleft != 0):
+            working_queue.append(check_node.childleft)
+            repeated_states.append(check_node.childleft.puzzle)
+            
+        if(check_node.childright != 0):
+            working_queue.append(check_node.childright)
+            repeated_states.append(check_node.childright.puzzle)
+        
+        queue_size = len(working_queue)
+        if(queue_size > max_queue_size):
+            max_queue_size = queue_size
+              
+        working_queue.sort(key = lambda x: ((x.depth + x.heur), x.depth))
+            
+
+            
+def print_puzzle(puzzle, dim):
+    for i in range(0, dim):
+        print(f"{puzzle[i]}")
 
 if __name__ == "__main__":
     main()
--- node.py
+++ node.py
@@ -4,14 +4,45 @@
 import copy
 
 class Node:
-    def __init__(self, puzzle, depth, cost):
+    def __init__(self, puzzle):
         self.puzzle = puzzle
-        self.depth = depth
-        self.cost = cost
+        self.depth = 0
+        self.heur = 0
+        self.childup = 0
+        self.childdown = 0
+        self.childleft = 0
+        self.childright = 0
         
-    def make_children(self):
+    def misplaced_tiles(self, goal):
+        rows = len(self.puzzle)
+        cols = len(self.puzzle[0])
+        count = 0
         
-        children = set()
+        for i in range(rows):
+            for j in range(cols):
+                if ((self.puzzle[i][j] != goal[i][j]) and (self.puzzle[i][j] != 0)):
+                    count+=1
+                    
+        return count
+                    
+    def manhattan(self, goal):
+        rows = len(self.puzzle)
+        cols = len(self.puzzle[0])
+        total = 0
+        
+        for i in range(rows):
+            for j in range(cols):
+                if ((self.puzzle[i][j] != goal[i][j]) and (self.puzzle[i][j] != 0)):
+                    temp1 = self.puzzle[i][j]
+                    for l in range(rows):
+                        for m in range(cols):
+                            if temp1 == goal[l][m]:
+                                total+=abs(l-i)
+                                total+=abs(m-j)
+        return total
+              
+    def make_children(self, expanded, heuristic, goal):
+        
         temp1 = []
         rows = len(self.puzzle)
         cols = len(self.puzzle[0])
@@ -27,70 +58,77 @@ class Node:
                     
         #up child
         if idrow != 0:
-            temp1 = self.puzzle
+            temp1 = copy.deepcopy(self.puzzle)
             temp1[idrow][idcol] = self.puzzle[idrow-1][idcol]
             temp1[idrow-1][idcol] = 0
-            children.add(Node(temp1, self.depth+1, self.cost+1))
+            if(temp1 not in expanded):
+                self.childup = (Node(temp1))
+                self.childup.depth = self.depth + 1
+                if(heuristic == 0):
+                    self.childup.heur = 0
+                if(heuristic == 1):
+                    self.childup.heur = self.childup.misplaced_tiles(goal)
+                if(heuristic == 2):
+                    self.childup.heur = self.childup.manhattan(goal)
             
         #down child
         if idrow != 2:
-            temp1 = self.puzzle
-            temp1[idrow][idcol] = self.puzzle[idrow+1][idcol]
-            temp1[idrow+1][idcol] = 0
-            children.add(Node(temp1, self.depth+1, self.cost+1))
+            temp2 = copy.deepcopy(self.puzzle)
+            temp2[idrow][idcol] = self.puzzle[idrow+1][idcol]
+            temp2[idrow+1][idcol] = 0
+            if(temp2 not in expanded):
+                self.childdown = (Node(temp2))
+                self.childdown.depth = self.depth + 1
+                if(heuristic == 0):
+                    self.childdown.heur = 0
+                if(heuristic == 1):
+                    self.childdown.heur = self.childdown.misplaced_tiles(goal)
+                if(heuristic == 2):
+                    self.childdown.heur = self.childdown.manhattan(goal)
             
         #left child
         if idcol != 0:
-            temp1 = self.puzzle
-            temp1[idrow][idcol] = self.puzzle[idrow][idcol-1]
-            temp1[idrow][idcol-1] = 0
-            children.add(Node(temp1, self.depth+1, self.cost+1))
+            temp3 = copy.deepcopy(self.puzzle)
+            temp3[idrow][idcol] = self.puzzle[idrow][idcol-1]
+            temp3[idrow][idcol-1] = 0
+            if(temp3 not in expanded):
+                self.childleft = (Node(temp3))
+                self.childleft.depth = self.depth + 1
+                if(heuristic == 0):
+                    self.childleft.heur = 0
+                if(heuristic == 1):
+                    self.childleft.heur = self.childleft.misplaced_tiles(goal)
+                if(heuristic == 2):
+                    self.childleft.heur = self.childleft.manhattan(goal)
             
         #right child
         if idcol != 2:
-            temp1 = self.puzzle
-            temp1[idrow][idcol] = self.puzzle[idrow][idcol+1]
-            temp1[idrow][idcol+1] = 0
-            children.add(Node(temp1, self.depth+1, self.cost+1)
+            temp4 = copy.deepcopy(self.puzzle)
+            temp4[idrow][idcol] = self.puzzle[idrow][idcol+1]
+            temp4[idrow][idcol+1] = 0
+            if(temp4 not in expanded):
+                self.childright = (Node(temp4))
+                self.childright.depth = self.depth + 1
+                if(heuristic == 0):
+                    self.childright.heur = 0
+                if(heuristic == 1):
+                    self.childright.heur = self.childright.misplaced_tiles(goal)
+                if(heuristic == 2):
+                    self.childright.heur = self.childright.manhattan(goal)
+                
         
-        return children
+        return
 
-        
-    def misplaced_tiles(self):
-        goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-        rows = len(self.puzzle)
-        cols = len(self.puzzle[0])
-        count = 0
-        
-        for i in range(rows):
-            for j in range(cols):
-                if ((self.puzzle[i][j] != goal[i][j]) and (self.puzzle[i][j] != 0)):
-                    count+=1
-                    
-        return count
-                    
-    def manhattan(self):
-        goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-        rows = len(self.puzzle)
-        cols = len(self.puzzle[0])
-        total = 0
-        
-        for i in range(rows):
-            for j in range(cols):
-                if ((self.puzzle[i][j] != goal[i][j]) and (self.puzzle[i][j] != 0)):
-                    temp1 = self.puzzle[i][j]
-                    for l in range(rows):
-                        for m in range(cols):
-                            if temp1 == goal[l][m]:
-                                total+=abs(l-i)
-                                total+=abs(m-j)
-        return total
-        
-    def solved(self):
-        goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    def solved(self, goal):
         if(goal == self.puzzle):
             return 1
         else:
             return 0 
-                                
+                  
+    def goal(dim):
+        #used matrix creation from - https://www.geeksforgeeks.org/python-matrix-creation-of-nn/
+        #set last element to 0 to match goal state
+        g = [list(range(1 + dim * i, 1 + dim * (i + 1))) for i in range(dim)]
+        g[dim-1][dim-1] = 0
+        return g              
         
